3ab00967ad6aa1ed1c0f174f0732fbe858eb2782
imranmahmud1122test-hue <imranmahmud1122.test@gmail.com>
2026-09-10 15:25:38 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -65,8 +65,9 @@ function aistudioMediaPlugin(): Plugin {
 // LINT.ThenChange(//depot/google3/java/com/google/alkali/boq/makersuite/applet_dev_service/templates/initializers/react_theme/vite.config.ts:aistudio_media_plugin)
 
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss(), aistudioMediaPlugin()],
+ return {
+  base: '/Ad-Master/',
+  plugins: [react(), tailwindcss(), aistudioMediaPlugin()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
